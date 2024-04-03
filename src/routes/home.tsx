c5728219212a8a0451efc4f3d69152274793799e
ricepotato <sukjun40@naver.com>
2024-04-03 21:46:21 +0900

    nav menu

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,15 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { auth } from "../firebase";
-
 export default function Home() {
-  const navigate = useNavigate();
-  const logOut = async () => {
-    await auth.signOut();
-    navigate("/login");
-  };
-  return (
-    <h1>
-      <button onClick={logOut}>Logout</button>
-    </h1>
-  );
+  return <h1>Home</h1>;
 }
